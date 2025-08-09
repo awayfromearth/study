eaad5d8ffff6b4d3a69e29a820aca2ad91e2e1ad
awayfromearth <zjchenmian@163.com>
2025-08-09 09:21:37 +0800

    20250809

---
+++ Back-end Development/Java/JavaSE/PracticeCode/liaoxuefeng/03_面向对象基础/09_包/oop-package/src/com/cm/sample/Main.java
@@ -0,0 +1,10 @@
+package com.cm.sample;
+
+import com.cm.world.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("小明");
+        person.sayHello();
+    }
+}
