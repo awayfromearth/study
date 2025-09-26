861c7f11f90434f8a6625a6805ae73112fd6a809
awayfromearth <zjchenmian@163.com>
2025-09-26 17:00:58 +0800

    20250926

---
+++ Back-end Development/Java/JavaSE/PracticeCode/liaoxuefeng/00_Java 程序基础/00_整数运算/Main.java
@@ -0,0 +1,10 @@
+// 计算前N个自然数的和
+public class Main {
+    public static void main(String[] args) {
+        int n = 100;
+        // TODO: sum = 1 + 2 + ... + n
+        int sum = (1 + n) * n / 2;
+        System.out.println(sum);
+        System.out.println(sum == 5050 ? "测试通过" : "测试失败");
+    }
+}
---
+++ Back-end Development/Java/JavaSE/PracticeCode/liaoxuefeng/05_Java 常用类/01_StringJoiner/Main.java
@@ -0,0 +1,23 @@
+/*
+* 请使用StringJoiner构造一个SELECT语句：
+* */
+import java.util.StringJoiner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] fields = { "name", "position", "salary" };
+        String table = "employee";
+        String select = buildSelectSql(table, fields);
+        System.out.println(select);
+        System.out.println("SELECT name, position, salary FROM employee".equals(select) ? "测试成功" : "测试失败");
+    }
+
+    static String buildSelectSql(String table, String[] fields) {
+        // TODO:
+        StringJoiner joiner = new StringJoiner(", ", "SELECT ", " FROM employee");
+        for (String field : fields) {
+            joiner.add(field);
+        }
+        return joiner.toString();
+    }
+}
