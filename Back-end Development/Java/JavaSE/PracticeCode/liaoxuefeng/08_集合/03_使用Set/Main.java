5596f5ed343a3bd6beede19cf0d576ef91cb2c1b
awayfromearth <zjchenmian@163.com>
2026-02-26 16:39:32 +0800

    20260226

---
+++ Back-end Development/Java/JavaSE/PracticeCode/liaoxuefeng/08_集合/03_使用Set/Main.java
@@ -0,0 +1,46 @@
+/**
+ * 在聊天软件中，发送方发送消息时，遇到网络超时后就会自动重发，因此，接收方可能会收到重复的消息，在显示给用户看的时候，需要首先去重。请练习使用Set去除重复的消息：
+ */
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Message> received = List.of(
+                new Message(1, "Hello!"),
+                new Message(2, "发工资了吗？"),
+                new Message(2, "发工资了吗？"),
+                new Message(3, "去哪吃饭？"),
+                new Message(3, "去哪吃饭？"),
+                new Message(4, "Bye")
+        );
+
+        List<Message> displayMessages = process(received);
+        for (Message message : displayMessages) {
+            System.out.println(message.text);
+        }
+    }
+
+    static List<Message> process(List<Message> received) {
+        Set<Integer> set = new HashSet<>();
+        List<Message> messages = new ArrayList<>();
+
+        for (Message message : received) {
+            if (set.add(message.sequence)) {
+                messages.add(message);
+            }
+        }
+
+        return messages;
+    }
+}
+
+class Message {
+    public final int sequence;
+
+    public final String text;
+
+    public Message(int sequence, String text) {
+        this.sequence = sequence;
+        this.text = text;
+    }
+}
